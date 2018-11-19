7162e856ee8bc6cb54ed449df76381bd23026093
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-19 19:54:10 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def unsafe?(speed)
   if speed < 40 || speed > 60
     true
@@ -7,6 +9,7 @@ end
 
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false
+	binding.pry
 end
 end
 	
